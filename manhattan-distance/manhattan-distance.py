2feff88b0679fc79435c0513ca68bf9d56118e35
Brandon Kelly <63052225+brand-kelly@users.noreply.github.com>
2026-05-23 17:15:05 -0500

    Add solution for manhattan-distance

---
+++ manhattan-distance/manhattan-distance.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def manhattan_distance(x, y):
+    """
+    Compute the Manhattan (L1) distance between vectors x and y.
+    Must return a float.
+    """
+    # Write code here
+    x_arr = np.array(x, dtype=float)
+    y_arr = np.array(y, dtype=float)
+
+    distance = np.sum(np.abs(x_arr - y_arr))
+    return distance
